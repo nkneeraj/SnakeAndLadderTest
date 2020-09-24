43c826d49278486417d5f174b7221d7b2582c1c9
nkneeraj <nkneerj641@gmail.com>
2020-09-24 21:21:16 +0530

    [NK]Created: UC2-Start position and no of players

--- src/com/CG/SnakeLadder/SnakeAndLadder.java
+++ src/com/CG/SnakeLadder/SnakeAndLadder.java
@@ -3,7 +3,7 @@ package com.CG.SnakeLadder;
 public class SnakeAndLadder {
 
 	public static void main(String[] args) {
-		System.out.println("Welcome to the game");
-	}
+		int start_position = 0;
+		int no_of_players = 1;		
 
 }
