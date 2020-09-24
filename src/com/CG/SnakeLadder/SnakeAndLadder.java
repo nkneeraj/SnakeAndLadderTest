e0ee2ae229c423941c8bcc9ed93bbfcf8f371a3a
nkneeraj <nkneerj641@gmail.com>
2020-09-24 21:58:56 +0530

    [NK]Created: uc6 - Logic for reaching exactly 100

--- src/com/CG/SnakeLadder/SnakeAndLadder.java
+++ src/com/CG/SnakeLadder/SnakeAndLadder.java
@@ -5,7 +5,7 @@ public class SnakeAndLadder {
 	public static void main(String[] args) {
 		int start_position = 0;
 		int no_of_players = 1;		
-		int dice_value = (int)Math.floor(Math.random()*10)%6;
+		
 
 		int current_position = start_position;
 
@@ -15,18 +15,21 @@ public class SnakeAndLadder {
 		int snake_value = 2;
 
 		while(current_position<100)
-		{
+		{	int dice_value = (int)Math.floor(Math.random()*10)%6;
 			int action_taken = (int)Math.floor(Math.random()*10)%3;
 			if(action_taken == no_play)
 				continue;
 			else if(action_taken == ladder_value)
+			{
 				current_position+=dice_value;
+				if(current_position > 100)
+					current_position-=dice_value;
+			}
 			else
 			{	
 				current_position-=dice_value;
 				if(current_position<0)
 					current_position=0;
 			}	
-		}
 	}
 }
