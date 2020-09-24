ff8a718d5dc96524f6f7b41b315105c451c047eb
nkneeraj <nkneerj641@gmail.com>
2020-09-24 23:02:12 +0530

    [NK]Created: uc8 - Snake and ladder game for 2 players

--- src/com/CG/SnakeLadder/SnakeAndLadder.java
+++ src/com/CG/SnakeLadder/SnakeAndLadder.java
@@ -3,37 +3,61 @@ package com.CG.SnakeLadder;
 public class SnakeAndLadder {
 
 	public static void main(String[] args) {
-		int start_position = 0;
-		int no_of_players = 1;		
-		
-
-		int current_position = start_position;
+		int p1_position = 0;
+		int p2_position = 0;
+		int no_of_players = 2;		
 		int no_play = 0;
 		int ladder_value = 1;
 		int snake_value = 2;
 		int counter = 0;
 
-		while(current_position<100)
-		{	int dice_value = (int)Math.floor(Math.random()*10)%6;
+		while(p1_position !=100 && p2_position !=100)
+		{	
+			int dice_value = (int)Math.floor(Math.random()*10)%6;
 			int action_taken = (int)Math.floor(Math.random()*10)%3;
 			counter++;
 			if(action_taken == no_play)
-				continue;
+				System.out.println("no play for player1");
 			else if(action_taken == ladder_value)
 			{
-				current_position+=dice_value;
-				if(current_position > 100)
-					current_position-=dice_value;
+				p1_position+=dice_value;
+				if(p1_position > 100)
+					p1_position-=dice_value;
 			}
 			else
 			{	
-				current_position-=dice_value;
-				if(current_position<0)
-					current_position=0;
+				p1_position-=dice_value;
+				if(p1_position<0)
+					p1_position=0;
 			}
 			
-			System.out.println("Counter:" +counter+ System.lineSeparator()+ "Current Position:"+ current_position + System.lineSeparator());
+			if(p1_position==100)
+				break;
+			else
+			{
+				dice_value = (int)Math.floor(Math.random()*10)%6;
+				action_taken = (int)Math.floor(Math.random()*10)%3;
+				if(action_taken == no_play)
+					System.out.println("no play for player1");
+				else if(action_taken == ladder_value)
+				{
+					p2_position+=dice_value;
+					if(p2_position > 100)
+						p2_position-=dice_value;
+				}
+				else
+				{	
+					p2_position-=dice_value;
+					if(p2_position<0)
+						p2_position=0;
+				}
+			}
+			System.out.println("Counter:" + counter + System.lineSeparator()+ "Player1 Position:"+ p1_position + System.lineSeparator()+ "player2 Position:"+ p2_position );
 		}
+		if(p1_position==100)
+			System.out.println("Player 1 is winner");
+		else
+			System.out.println("Player 2 is winner");
 			
 	}
 }
