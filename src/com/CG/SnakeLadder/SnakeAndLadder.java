b55c181c62ef5a3686a3f053c884a6446b58e4fa
nkneeraj <nkneerj641@gmail.com>
2020-09-24 21:27:20 +0530

    [NK]Created: uc3-Random Dice Value Generated

--- src/com/CG/SnakeLadder/SnakeAndLadder.java
+++ src/com/CG/SnakeLadder/SnakeAndLadder.java
@@ -5,5 +5,6 @@ public class SnakeAndLadder {
 	public static void main(String[] args) {
 		int start_position = 0;
 		int no_of_players = 1;		
-
+		int dice_value = (int)Math.floor(Math.random()*10)%6;
+}
 }
