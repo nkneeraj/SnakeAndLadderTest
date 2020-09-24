2725cc7cdd4bf2461109889c7639ddc4ab712f96
nkneeraj <nkneerj641@gmail.com>
2020-09-24 21:46:10 +0530

    [NK]Created: uc4 - no_play,ladder,snake condition set

--- src/com/CG/SnakeLadder/SnakeAndLadder.java
+++ src/com/CG/SnakeLadder/SnakeAndLadder.java
@@ -6,5 +6,20 @@ public class SnakeAndLadder {
 		int start_position = 0;
 		int no_of_players = 1;		
 		int dice_value = (int)Math.floor(Math.random()*10)%6;
+		
+		int current_position = start_position;
+		int action_taken = (int)Math.floor(Math.random()*10)%3;
+		
+		int no_play = 0;
+		int ladder_value = 1;
+		int snake_value = 2;
+		
+		if(action_taken == no_play)
+			return;
+		else if(action_taken == ladder_value)
+			current_position+=dice_value;
+		else 
+			current_position-=dice_value;
+		
 }
 }
